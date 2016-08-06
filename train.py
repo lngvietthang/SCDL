c81226e37bc7fa30c2d3747e8d5a26b32a9d65fb
nhittt <thaonhitt2005@gmail.com>
2016-08-06 10:34:12 +0700

    Add files via upload

--- train.py
+++ train.py
@@ -29,43 +29,44 @@ if not MODEL_OUTPUT_FILE:
     original_sentence_text, compression_sentence_text) = load_data_from_json2(INPUT_DATA_FILE,  0.2, VOCABULARY_SIZE)
 # Build model
 print '\nBuild model'
-model = GRUTheano(VOCABULARY_SIZE, hidden_dim=HIDDEN_DIM, bptt_truncate=-1)
-#model = load_model_parameters_theano('./data/pretrained.npz')
+#model = GRUTheano(VOCABULARY_SIZE, hidden_dim=HIDDEN_DIM, bptt_truncate=-1)
+model = load_model_parameters_theano('/home/nhitt/Dropbox/Compression_lstm/rnn-tutorial-gru-lstm/GRU-2016-08-05-13-48-2000-50-100.dat.npz')
 
 #Print SGD step time
 print 'S2q'
-t1 = time.time()
-model.sgd_step(X_train[10], y_train[10], LEARNING_RATE)
-c = model.ce_error(X_train[10], y_train[10])
-print ('loss x[10]: %f'%c)
-t2 = time.time()
-print "SGD Step time: %f milliseconds" % ((t2 - t1) * 1000.)
-sys.stdout.flush()
-
-# We do this every few examples to understand what's going on
-def sgd_callback(model, num_examples_seen):
-  dt = datetime.now().isoformat()
-  loss = model.calculate_loss(X_train[:1000], y_train[:1000])
-  print("\n%s (%d)" % (dt, num_examples_seen))
-  print("--------------------------------------------------")
-  print("Loss: %f" % loss)
-  save_model_parameters_theano(model, MODEL_OUTPUT_FILE)
-  print("\n")
-  sys.stdout.flush()
-
-t3 = time.time()
-for epoch in range(NEPOCH):
-  print('Epoch %d: ' % epoch)
-  train_with_sgd(model, X_train, y_train, learning_rate=LEARNING_RATE, nepoch=1, decay=0.9,
-    callback_every=PRINT_EVERY, callback=sgd_callback)
-t4 = time.time()
-print "SGD Train time: %f" % ((t4 - t3))
-sys.stdout.flush()
-
+# t1 = time.time()
+# model.sgd_step(X_train[10], y_train[10], LEARNING_RATE)
+# c = model.ce_error(X_train[10], y_train[10])
+# print ('loss x[10]: %f'%c)
+# t2 = time.time()
+# print "SGD Step time: %f milliseconds" % ((t2 - t1) * 1000.)
+# sys.stdout.flush()
+#
+# # We do this every few examples to understand what's going on
+# def sgd_callback(model, num_examples_seen):
+#   dt = datetime.now().isoformat()
+#   loss = model.calculate_loss(X_train[:1000], y_train[:1000])
+#   print("\n%s (%d)" % (dt, num_examples_seen))
+#   print("--------------------------------------------------")
+#   print("Loss: %f" % loss)
+#   save_model_parameters_theano(model, MODEL_OUTPUT_FILE)
+#   print("\n")
+#   sys.stdout.flush()
+#
+# t3 = time.time()
+# for epoch in range(NEPOCH):
+#   print('Epoch %d: ' % epoch)
+#   train_with_sgd(model, X_train, y_train, learning_rate=LEARNING_RATE, nepoch=1, decay=0.9,
+#     callback_every=PRINT_EVERY, callback=sgd_callback)
+# t4 = time.time()
+# print "SGD Train time: %f" % ((t4 - t3))
+# sys.stdout.flush()
+#
 print 'Testing...'
 predict_test = testing(model, X_test)
 np.save("GRU-%s-%s-%s-%s.predict" % (ts, VOCABULARY_SIZE, EMBEDDING_DIM, HIDDEN_DIM), predict_test)
-#print 'Compute f1:...'
-#f1 = compute_f1(y_test, predict_test)
-#write_output('./Output_seq2seq_theano', f1[5], original_sentence_text, compression_sentence_text, 0.6)
+
+print 'Compute f1:...'
+f1 = compute_f1(y_test, predict_test)
+write_output('./Output_seq2seq_theano', f1[5], original_sentence_text, compression_sentence_text, 0.6)
 
