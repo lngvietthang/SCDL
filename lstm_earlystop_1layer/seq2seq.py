2e500a8968ae1c937a501d0f5b3111eb34a610fa
nhittt <thaonhitt2005@gmail.com>
2016-08-19 15:20:52 +0700

    Add files via upload

--- lstm_earlystop_1layer/seq2seq.py
+++ lstm_earlystop_1layer/seq2seq.py
@@ -32,6 +32,9 @@ class GRUTheano:
         V = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (3, hidden_dim))
         b = np.zeros((4, hidden_dim))
         c = np.zeros(3)
+        U_decode = np.random.uniform(-np.sqrt(1. / hidden_dim), np.sqrt(1. / hidden_dim), (4, hidden_dim, hidden_dim))
+        W_decode = np.random.uniform(-np.sqrt(1. / hidden_dim), np.sqrt(1. / hidden_dim), (4, hidden_dim, hidden_dim))
+        b_decode = np.zeros((4, hidden_dim))
         # Theano: Created shared variables
         self.E = theano.shared(name='E', value=E.astype(theano.config.floatX))
         self.Ey_encode = theano.shared(name='Ey_encode', value=Ey_encode.astype(theano.config.floatX))
@@ -41,6 +44,9 @@ class GRUTheano:
         self.V = theano.shared(name='V', value=V.astype(theano.config.floatX))
         self.b = theano.shared(name='b', value=b.astype(theano.config.floatX))
         self.c = theano.shared(name='c', value=c.astype(theano.config.floatX))
+        self.U_decode = theano.shared(name='U_decode', value=U_decode.astype(theano.config.floatX))
+        self.W_decode = theano.shared(name='W_decode', value=W_decode.astype(theano.config.floatX))
+        self.b_decode = theano.shared(name='b_decode', value=b_decode.astype(theano.config.floatX))
         # SGD / rmsprop: Initialize parameters
         self.mE = theano.shared(name='mE', value=np.zeros(E.shape).astype(theano.config.floatX))
         self.mEy_encode = theano.shared(name='mEy_encode', value=np.zeros(Ey_encode.shape).astype(theano.config.floatX))
@@ -50,12 +56,15 @@ class GRUTheano:
         self.mW = theano.shared(name='mW', value=np.zeros(W.shape).astype(theano.config.floatX))
         self.mb = theano.shared(name='mb', value=np.zeros(b.shape).astype(theano.config.floatX))
         self.mc = theano.shared(name='mc', value=np.zeros(c.shape).astype(theano.config.floatX))
+        self.mU_decode = theano.shared(name='mU_decode', value=np.zeros(U_decode.shape).astype(theano.config.floatX))
+        self.mW_decode = theano.shared(name='mW_decode', value=np.zeros(W_decode.shape).astype(theano.config.floatX))
+        self.mb_decode = theano.shared(name='mb_decode', value=np.zeros(b_decode.shape).astype(theano.config.floatX))
         # We store the Theano graph here
         self.theano = {}
         self.__theano_build__()
 
     def __theano_build__(self):
-        E, Ey_encode, Ey_decode, V, U, W, b, c = self.E, self.Ey_encode, self.Ey_decode, self.V, self.U, self.W, self.b, self.c
+        E, Ey_encode, Ey_decode, V, U, W, b, c, U_decode, W_decode, b_decode = self.E, self.Ey_encode, self.Ey_decode, self.V, self.U, self.W, self.b, self.c, self.U_decode, self.W_decode, self.b_decode
 
         x = T.ivector('x')
         y = T.ivector('y')
@@ -94,10 +103,10 @@ class GRUTheano:
             xy_e_d = theano.tensor.concatenate([x_e, y_e], axis=0)
 
             # Decode   #LSTM Layer 1
-            i_t1_d = T.nnet.hard_sigmoid(U[0].dot(xy_e_d) + W[0].dot(s_t1_prev_d) + b[0])
-            f_t1_d = T.nnet.hard_sigmoid(U[1].dot(xy_e_d) + W[1].dot(s_t1_prev_d) + b[1])
-            o_t1_d = T.nnet.hard_sigmoid(U[2].dot(xy_e_d) + W[2].dot(s_t1_prev_d) + b[2])
-            g_t1_d = T.tanh(U[3].dot(xy_e_d) + W[3].dot(s_t1_prev_d) + b[3])
+            i_t1_d = T.nnet.hard_sigmoid(U_decode[0].dot(xy_e_d) + W_decode[0].dot(s_t1_prev_d) + b_decode[0])
+            f_t1_d = T.nnet.hard_sigmoid(U_decode[1].dot(xy_e_d) + W_decode[1].dot(s_t1_prev_d) + b_decode[1])
+            o_t1_d = T.nnet.hard_sigmoid(U_decode[2].dot(xy_e_d) + W_decode[2].dot(s_t1_prev_d) + b_decode[2])
+            g_t1_d = T.tanh(U_decode[3].dot(xy_e_d) + W_decode[3].dot(s_t1_prev_d) + b_decode[3])
             c_t1_d = c_t1_prev_d * f_t1_d + g_t1_d * i_t1_d
             s_t1_d = T.tanh(c_t1_d) * o_t1_d
 
@@ -118,10 +127,10 @@ class GRUTheano:
             xy_e_d_test = theano.tensor.concatenate([x_e, o_t_pre_test], axis=0)
 
             # Decode   #LSTM Layer 1
-            i_t1_d_test = T.nnet.hard_sigmoid(U[0].dot(xy_e_d_test) + W[0].dot(s_t1_prev_d_test) + b[0])
-            f_t1_d_test = T.nnet.hard_sigmoid(U[1].dot(xy_e_d_test) + W[1].dot(s_t1_prev_d_test) + b[1])
-            o_t1_d_test = T.nnet.hard_sigmoid(U[2].dot(xy_e_d_test) + W[2].dot(s_t1_prev_d_test) + b[2])
-            g_t1_d_test = T.tanh(U[3].dot(xy_e_d_test) + W[3].dot(s_t1_prev_d_test) + b[3])
+            i_t1_d_test = T.nnet.hard_sigmoid(U_decode[0].dot(xy_e_d_test) + W_decode[0].dot(s_t1_prev_d_test) + b_decode[0])
+            f_t1_d_test = T.nnet.hard_sigmoid(U_decode[1].dot(xy_e_d_test) + W_decode[1].dot(s_t1_prev_d_test) + b_decode[1])
+            o_t1_d_test = T.nnet.hard_sigmoid(U_decode[2].dot(xy_e_d_test) + W_decode[2].dot(s_t1_prev_d_test) + b_decode[2])
+            g_t1_d_test = T.tanh(U_decode[3].dot(xy_e_d_test) + W_decode[3].dot(s_t1_prev_d_test) + b_decode[3])
             c_t1_d_test = c_t1_prev_d_test * f_t1_d_test + g_t1_d_test * i_t1_d_test
             s_t1_d_test = T.tanh(c_t1_d_test) * o_t1_d_test
 
@@ -166,12 +175,15 @@ class GRUTheano:
         db = T.grad(cost, b)
         dV = T.grad(cost, V)
         dc = T.grad(cost, c)
+        dU_decode = T.grad(cost, U_decode)
+        dW_decode = T.grad(cost, W_decode)
+        db_decode = T.grad(cost, b_decode)
 
         # Assign functions
         self.predict = theano.function([x], o_test)
         self.predict_class = theano.function([x], prediction)
         self.ce_error = theano.function([x, y], cost)
-        self.bptt = theano.function([x, y], [dE, dU, dW, db, dV, dc])
+        self.bptt = theano.function([x, y], [dE, dU, dW, db, dV, dc, dU_decode, dW_decode, db_decode])
 
         # SGD parameters
         learning_rate = T.scalar('learning_rate')
@@ -184,6 +196,9 @@ class GRUTheano:
         mV = decay * self.mV + (1 - decay) * dV ** 2
         mb = decay * self.mb + (1 - decay) * db ** 2
         mc = decay * self.mc + (1 - decay) * dc ** 2
+        mU_decode = decay * self.mU_decode + (1 - decay) * dU_decode ** 2
+        mW_decode = decay * self.mW_decode + (1 - decay) * dW_decode ** 2
+        mb_decode = decay * self.mb_decode + (1 - decay) * db_decode ** 2
 
         self.sgd_step = theano.function(
             [x, y, learning_rate, theano.Param(decay, default=0.9)],
@@ -194,12 +209,18 @@ class GRUTheano:
                      (V, V - learning_rate * dV / T.sqrt(mV + 1e-6)),
                      (b, b - learning_rate * db / T.sqrt(mb + 1e-6)),
                      (c, c - learning_rate * dc / T.sqrt(mc + 1e-6)),
+                     (U_decode, U_decode - learning_rate * dU_decode / T.sqrt(mU_decode + 1e-6)),
+                     (W_decode, W_decode - learning_rate * dW_decode / T.sqrt(mW_decode + 1e-6)),
+                     (b_decode, b_decode - learning_rate * db_decode / T.sqrt(mb_decode + 1e-6)),
                      (self.mE, mE),
                      (self.mU, mU),
                      (self.mW, mW),
                      (self.mV, mV),
                      (self.mb, mb),
-                     (self.mc, mc)
+                     (self.mc, mc),
+                     (self.mU_decode, mU_decode),
+                     (self.mW_decode, mW_decode),
+                     (self.mb_decode, mb_decode)
                     ])
 
 
@@ -234,12 +255,15 @@ def save_model_parameters_theano(model, outfile):
         W=model.W.get_value(),
         V=model.V.get_value(),
         b=model.b.get_value(),
-        c=model.c.get_value())
+        c=model.c.get_value(),
+        U_decode=model.U_decode.get_value(),
+        W_decode=model.W_decode.get_value(),
+        b_decode=model.b_decode.get_value())
     print "Saved model parameters to %s." % outfile
 
 def load_model_parameters_theano(path, modelClass=GRUTheano):
     npzfile = np.load(path)
-    E, Ey_encode, Ey_decode, U, W, V, b, c = npzfile["E"], npzfile["Ey_encode"],npzfile["Ey_decode"],npzfile["U"], npzfile["W"], npzfile["V"], npzfile["b"], npzfile["c"]
+    E, Ey_encode, Ey_decode, U, W, V, b, c = npzfile["E"], npzfile["Ey_encode"],npzfile["Ey_decode"],npzfile["U"], npzfile["W"], npzfile["V"], npzfile["b"], npzfile["c"], npzfile["U_decode"], npzfile["W_decode"], npzfile["b_decode"]
     hidden_dim, word_dim = E.shape[0]+3, E.shape[1]
     print "Building model model from %s with hidden_dim=%d word_dim=%d" % (path, hidden_dim, word_dim)
     sys.stdout.flush()
@@ -250,4 +274,7 @@ def load_model_parameters_theano(path, modelClass=GRUTheano):
     model.V.set_value(V)
     model.b.set_value(b)
     model.c.set_value(c)
+    model.U_decode.set_value(U_decode)
+    model.W_decode.set_value(W_decode)
+    model.b_decode.set_value(b_decode)
     return model
